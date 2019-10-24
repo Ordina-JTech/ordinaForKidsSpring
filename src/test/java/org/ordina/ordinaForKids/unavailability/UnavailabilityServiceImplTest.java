fe8d6c82272a50694a487f2339763358a98551ad
Michels <Thomas.Michels@ordina.nl>
2019-10-24 13:54:51 +0200

    begin gemaakt met test

--- src/test/java/org/ordina/ordinaForKids/unavailability/UnavailabilityServiceImplTest.java
+++ src/test/java/org/ordina/ordinaForKids/unavailability/UnavailabilityServiceImplTest.java
@@ -1,12 +1,26 @@
 package org.ordina.ordinaForKids.unavailability;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ordina.ordinaForKids.calendarEvent.CalendarEvent;
+import org.ordina.ordinaForKids.calendarEvent.CalendarEventServiceImpl;
+import org.ordina.ordinaForKids.calendarEvent.CalendarEventTestHelper;
+import org.ordina.ordinaForKids.validation.UnavailabilityNotFoundException;
 import org.ordina.ordinaForKids.validation.UnavailabilityOverlapsEventException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,26 +29,78 @@ public class UnavailabilityServiceImplTest {
 
 	@Autowired
 	UnavailabilityServiceImpl unavailabilityService;
+
+	@MockBean
+	UnavailabilityRepository mockUnavailabilityRepo;
+	@MockBean
+	CalendarEventServiceImpl calendarEventService;
+
+	List<Unavailability> mockUnavailabilities;
+
+	@Before
+	public void setup() {
+		mockUnavailabilities = UnavailabilityTestHelper.createUnavailabilityList();
+	}
+
+	@Test
+	public void getAllUnavailabilitiesTest() throws UnavailabilityNotFoundException {
+		// arrange
+		when(mockUnavailabilityRepo.findAll()).thenReturn(mockUnavailabilities);
+		int nrOfMockUnavailabilities = mockUnavailabilities.size(); 
+
+		// act
+		List<Unavailability> unavailabilities = unavailabilityService.getAllUnavailabilities();
+
+		// assert
+		int nrOfReturnedUnavailabilities = unavailabilities.size();
+		assertEquals(nrOfMockUnavailabilities, nrOfReturnedUnavailabilities);
+	}
+
+	@Test(expected = UnavailabilityNotFoundException.class)
+	public void getAllUnavailabilitiesTestIfNoAvailabilitiesStored() throws UnavailabilityNotFoundException {
+		// arrange
+		List<Unavailability> emptyList = new ArrayList<Unavailability>();
+		when(mockUnavailabilityRepo.findAll()).thenReturn(emptyList);
+		
+		// act
+		unavailabilityService.getAllUnavailabilities();
+
+		// assert
+		// UnavailabilityNotFoundException should be thrown, as mock repository returns empty list
+	}
+
 	
 	@Test
-	public void testCreateUnavailability() throws UnavailabilityOverlapsEventException {
-		Unavailability u = UnavailabilityTestHelper.createUnavailability();
-		unavailabilityService.createUnavailability(u);
-	}	
+	public void createUnavailabilityTest() throws UnavailabilityOverlapsEventException {
+		// arrange
+		List<CalendarEvent> events =  CalendarEventTestHelper.getMockCalendarEvents(3);
+		when(calendarEventService.getCalendarEvents()).thenReturn(events);
+		Unavailability newUnavailability = UnavailabilityTestHelper.createUnavailabilityList().get(0);		
+		LocalDate nonOverlappingDate = LocalDate.of(2010, 1, 1);
+		newUnavailability.setDate(nonOverlappingDate);
+		
+		// act
+		unavailabilityService.createUnavailability(newUnavailability);
 
-	@Test
-	public void testDeleteUnavailability() {
-		fail("Not yet implemented");
+		// assert
+		verify(mockUnavailabilityRepo).save(newUnavailability);
 	}
-
+	
 	@Test
-	public void testGetUnavailability() {
-		fail("Not yet implemented");
-	}
+	public void createUnavailabilityTestWithOverlappingDate() throws UnavailabilityOverlapsEventException {
+		// arrange
+		List<CalendarEvent> events =  CalendarEventTestHelper.getMockCalendarEvents(3);
+		when(calendarEventService.getCalendarEvents()).thenReturn(events);
+		Unavailability newUnavailability = UnavailabilityTestHelper.createUnavailabilityList().get(0);		
+		LocalDate overlappingDate = events.get(0).getDate(); 
+		newUnavailability.setDate(overlappingDate);
+		
+		
+		// act
+		unavailabilityService.createUnavailability(newUnavailability);
 
-	@Test
-	public void testGetAllUnavailabilities() {
-		fail("Not yet implemented");
+		// assert
+		verify(mockUnavailabilityRepo).save(newUnavailability);
 	}
-
+	
 }
--- src/test/java/org/ordina/ordinaForKids/unavailability/UnavailabilityTestHelper.java
+++ src/test/java/org/ordina/ordinaForKids/unavailability/UnavailabilityTestHelper.java
@@ -1,18 +1,26 @@
 package org.ordina.ordinaForKids.unavailability;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.ordina.ordinaForKids.teachingModule.TeachingModules;
 
 public class UnavailabilityTestHelper {
 
-	protected static Unavailability createUnavailability(){
-		Unavailability unavailability = new Unavailability();
-		unavailability.setDate(new Date());
-		unavailability.setLoggedBy("demo@user.com");
-		unavailability.setReason("vakantie");
-		unavailability.setUnavailableModule(TeachingModules.AGILE);
-		return unavailability;
+	protected static final int NUMBER_OF_MOCK_UNAVAILABILITIES = 10;
+
+	protected static List<Unavailability> createUnavailabilityList() {
+		List<Unavailability> unavailabilities = new ArrayList<>();
+		for (int i = 0; NUMBER_OF_MOCK_UNAVAILABILITIES > i; i++) {
+			Unavailability unavailability = new Unavailability();
+			unavailability.setDate(LocalDate.of(2019, i + 1, i + 10));
+			unavailability.setLoggedBy("demo" + i + "@user.com");
+			unavailability.setReason("vakantie");
+			unavailability.setUnavailableModule(TeachingModules.AGILE);
+			unavailabilities.add(unavailability);
+		}
+		return unavailabilities;
 	}
 
 }
